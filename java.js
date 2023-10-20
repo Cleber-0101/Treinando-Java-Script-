415a8ded18a6704ac727ed6b5d64fb619b794780
Cleber-esx <cleber.batista@esx.com.br>
2023-10-20 15:53:34 -0300

    Adicionando funções

--- java.js
+++ java.js
@@ -6,7 +6,7 @@ function entrar() {
 
   if(nome === '' || nome === null ){
     alert("Ops algo de errado aconteceu")
-    area.innerHTML = "Clique no botão para acessar...."
+     area.innerHTML = "Clique no botão para acessar...."
   }else{
     area.innerHTML = "Bem vindo " + nome + " ";
 
@@ -22,4 +22,21 @@ function entrar() {
 function sair(){
     alert("Ate mais !")
     area.innerHTML = "Voce saiu !"
+}
+
+
+function mediaAluno(nota, nota2) {
+    var media = (nota + nota2) / 2
+
+    if (media >= 7) {
+        console.log("Aluno aprovado com a media: "+  media)
+    } else if(media < 7){
+        console.log("Aluno reprovado com a média: " + media)
+    }
+}
+
+
+function aluno(nome ,curso) {
+    var mensagem = "Seja bem vindo " + nome + " ao curso de " + curso;
+    console.log(mensagem);
 }
